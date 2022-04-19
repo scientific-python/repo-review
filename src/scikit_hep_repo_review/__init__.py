527ab3d809d7b06b6863f00c18eed264ba057ad4
Henry Schreiner <henryschreineriii@gmail.com>
2022-04-19 15:45:12 -0400

    chore: bump to version 0.2.3
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/scikit_hep_repo_review/__init__.py
+++ src/scikit_hep_repo_review/__init__.py
@@ -7,6 +7,6 @@ scikit-hep-repo-review: Review repos for compliance to the Scikit-HEP developer 
 
 from __future__ import annotations
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 __all__ = ("__version__",)
