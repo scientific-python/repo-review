02ced7f233c4c92fac18bc15b78af91640afb6ef
Henry Schreiner <HenrySchreinerIII@gmail.com>
2022-04-29 00:49:05 -0600

    chore: even better version number

--- src/scikit_hep_repo_review/__init__.py
+++ src/scikit_hep_repo_review/__init__.py
@@ -7,6 +7,6 @@ scikit-hep-repo-review: Review repos for compliance to the Scikit-HEP developer 
 
 from __future__ import annotations
 
-__version__ = "0.2.5"
+__version__ = "0.2.4"
 
 __all__ = ("__version__",)
