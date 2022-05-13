8601dd49d3d478170d31646a6b16b05caeba3891
Henry Schreiner <HenrySchreinerIII@gmail.com>
2022-05-13 11:23:23 -0400

    docs: update webapp pin

--- docs/webapp.js
+++ docs/webapp.js
@@ -115,7 +115,7 @@ async function prepare_pyodide() {
   await pyodide.loadPackage("micropip");
   await pyodide.runPythonAsync(`
         import micropip
-        await micropip.install(["scikit_hep_repo_review==0.2.4"])
+        await micropip.install(["scikit_hep_repo_review==0.3.0"])
     `);
   return pyodide;
 }
