89b967b940d51d01fd2576bfdd8b9b8e84de95e7
Henry Schreiner <HenrySchreinerIII@gmail.com>
2023-06-03 17:34:56 -0400

    chore: bump version to 0.7.0b2

--- src/repo_review/__init__.py
+++ src/repo_review/__init__.py
@@ -7,6 +7,6 @@ repo-review: Review repos with a set of checks defined by plugins.
 
 from __future__ import annotations
 
-__version__ = "0.7.0b1"
+__version__ = "0.7.0b2"
 
 __all__ = ["__version__"]
