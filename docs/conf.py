70413828c5ef1ef237bacc116278490a717de634
Henry Schreiner <HenrySchreinerIII@gmail.com>
2023-06-07 13:45:46 -0400

    docs: add version number (#92)
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,27 @@
+import importlib.metadata
+
 project = "repo-review"
 copyright = "2022, Henry Schreiner"
 author = "Henry Schreiner"
+release = importlib.metadata.version("repo-review")
 
 extensions = [
     "myst_parser",
     "sphinxcontrib.programoutput",
+    "sphinxext.opengraph",
+    "sphinx_copybutton",
 ]
 
 source_suffix = [".rst", ".md"]
+
+exclude_patterns = [
+    "_build",
+    "**.ipynb_checkpoints",
+    "Thumbs.db",
+    ".DS_Store",
+    ".env",
+    ".venv",
+]
 
 html_theme = "furo"
 
