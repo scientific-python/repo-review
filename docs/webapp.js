bfcb802d671c9c9847bbc2b41f3c7362276cb505
Henry Schreiner <henryschreineriii@gmail.com>
2023-06-14 15:37:53 -0400

    docs: bump to 0.7.0b10
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- docs/webapp.js
+++ docs/webapp.js
@@ -260,7 +260,7 @@ class App extends React.Component {
           name: val.name.toString(),
           description: val.description.toString(),
           state: val.result,
-          err_msg: val.err_markdown().toString(),
+          err_msg: val.err_as_html().toString(),
           url: val.url.toString(),
         });
       }
