703344169689fa7cbe15aa515b046260ed5480f1
Henry Schreiner <HenrySchreinerIII@gmail.com>
2024-08-19 17:42:00 -0400

    fix: support object structure in schema (#231)
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/test_package.py
+++ tests/test_package.py
@@ -43,8 +43,43 @@ def test_broken_validate_pyproject(tmp_path: Path) -> None:
     results = process(tmp_path)
 
     (result,) = (r for r in results.results if r.name == "VPP001")
-    assert "must match pattern" in result.err_msg
+    assert "must be valid exactly by one definition" in result.err_msg
     assert not result.result
+
+
+def test_broken_validate_pyproject_object(tmp_path: Path) -> None:
+    pytest.importorskip("validate_pyproject")
+    tmp_path.joinpath("pyproject.toml").write_text(
+        textwrap.dedent(
+            """\
+                [tool.repo-review.ignore]
+                a2 = "some message"
+            """
+        )
+    )
+
+    results = process(tmp_path)
+
+    (result,) = (r for r in results.results if r.name == "VPP001")
+    assert "must be valid exactly by one definition" in result.err_msg
+    assert not result.result
+
+
+def test_working_validate_pyproject_object(tmp_path: Path) -> None:
+    pytest.importorskip("validate_pyproject")
+    tmp_path.joinpath("pyproject.toml").write_text(
+        textwrap.dedent(
+            """\
+                [tool.repo-review.ignore]
+                PP102 = "some message"
+            """
+        )
+    )
+
+    results = process(tmp_path)
+
+    (result,) = (r for r in results.results if r.name == "VPP001")
+    assert result.result
 
 
 def test_testing_function():
